190e6727384e61264f602cc69eb6ebcd1db8c196
bintanghawley <kanaiakasa@gmail.com>
2026-05-24 16:05:08 +0700

    final push

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
 
-cd /d C:\bsa-management-internal-system\app
+cd /d C:\bsa-internal-management-system\app
 
 start http://bsa.system:8000
 
-C:\bsa-management-internal-system\php\php.exe artisan serve --host=0.0.0.0 --port=8000
+C:\bsa-internal-management-system\php\php.exe artisan serve --host=0.0.0.0 --port=8000
 
 pause
